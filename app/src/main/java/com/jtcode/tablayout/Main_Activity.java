c2f52b2e6f16271caa13ddf8b636901e2bbfa786
Julián Troyano <juliantrodom@gmail.com>
2016-11-30 14:46:44 +0100

    fist commit

---
+++ app/src/main/java/com/jtcode/tablayout/Main_Activity.java
@@ -0,0 +1,67 @@
+package com.jtcode.tablayout;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.jtcode.tablayout.adapters.ViewPageAdapter;
+
+public class Main_Activity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPageAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_);
+        toolbar=(Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        //inicializar el tabLayout y crear la pestaña
+        tabLayout=(TabLayout)findViewById(R.id.tablayout);
+
+        for(String title : getResources().getStringArray(R.array.tabs))
+            tabLayout.addTab(tabLayout.newTab().setText(title));
+
+        viewPager=(ViewPager)findViewById(R.id.viewp);
+        adapter=new ViewPageAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/jtcode/tablayout/adapters/ViewPageAdapter.java
@@ -0,0 +1,57 @@
+package com.jtcode.tablayout.adapters;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.jtcode.tablayout.CustomFragment;
+
+/**
+ * Created by usuario on 30/11/16.
+ */
+
+public class ViewPageAdapter extends FragmentStatePagerAdapter {
+
+    private int tabCount;
+    public ViewPageAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount=tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        CustomFragment customFragment=null;
+        Bundle bundle= new Bundle();
+
+        switch (position){
+            case 0:
+                bundle.putString(CustomFragment.KEY_REG_TEXT,"Tab 1");
+                customFragment= (CustomFragment) CustomFragment.newInstance(bundle);
+                break;
+            case 1:
+                bundle.putString(CustomFragment.KEY_REG_TEXT,"Tab 2");
+                customFragment= (CustomFragment) CustomFragment.newInstance(bundle);
+                break;
+            case 2:
+                bundle.putString(CustomFragment.KEY_REG_TEXT,"Tab 3");
+                customFragment= (CustomFragment) CustomFragment.newInstance(bundle);
+                break;
+            case 3:
+                bundle.putString(CustomFragment.KEY_REG_TEXT,"Tab 4");
+                customFragment= (CustomFragment) CustomFragment.newInstance(bundle);
+                break;
+            case 4:
+                bundle.putString(CustomFragment.KEY_REG_TEXT,"Tab 5");
+                customFragment=(CustomFragment) CustomFragment.newInstance(bundle);
+                break;
+
+        }
+        return customFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
